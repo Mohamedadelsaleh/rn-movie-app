50b83f959a13c8b6647e3e77c000a076be457ddd
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2025-10-10 00:21:53 +0300

    Home Screen

---
+++ components/SearchBar.tsx
@@ -0,0 +1,38 @@
+import { Image, TextInput, View } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+
+interface Props {
+    onPress?: () => void
+    placeholder :string
+}
+
+const SearchBar = ({
+    onPress,
+    placeholder
+} : Props) => {
+  return (
+    <View
+        className='flex-row items-center px-5 py-2 rounded-full bg-dark-200'
+    >
+      <Image 
+        source={icons.search}
+        className='size-5'
+        resizeMode='contain'
+        tintColor="#ab8bff"
+      />
+
+      <TextInput 
+        onPress={onPress}
+        placeholder={placeholder}
+        value=''
+        onChangeText={() => {}}
+        placeholderTextColor="#ab8bff"
+        className='flex-1 ml-2 text-white'
+      />
+    </View>
+  )
+}
+
+export default SearchBar;
